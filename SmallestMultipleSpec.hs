e5294a761eed3983286cbdcb8db3c0df83115678
Paulo Sousa <pauloj10@gmail.com>
2015-09-01 23:57:11 -0300

    updated spec format

--- SmallestMultipleSpec.hs
+++ SmallestMultipleSpec.hs
@@ -7,7 +7,7 @@ main :: IO()
 main = hspec $
     describe "number multiples" $ do
         it "return true when check if 720 is multiple of the numbers from 1 to 6" $
-            True `shouldBe` isMultipleOfAll 720 [1..6]
+            isMultipleOfAll 720 [1..6] `shouldBe` True
         it "return the smallest number that can be divided by each number from 1 to 10" $
             smallestMultiple 10 `shouldBe` 2520
         it "return the smallest number that can be divided by each number from 1 to 20" $
